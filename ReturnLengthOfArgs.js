f385e7b441fd149d2cac9d6a11b60a8c00ea0cfa
Raja V <rajamails19@gmail.com>
2023-12-31 15:45:04 -0500

    Changes

--- ReturnLengthOfArgs.js
+++
@@ -1,7 +0,0 @@
-var argumentsLength = function(...args) {
-    // Return the length of the args array.
-    return args.length;
-};
-
-// Example usage:
-console.log(argumentsLength(1, 2, 3)); // Outputs: 3
